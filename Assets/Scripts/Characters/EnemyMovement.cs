35a7d67b6295ad493189626d8955a397a7ec68eb
Cidney Hamilton <cidney@cidneyhamilton.com>
2019-10-15 13:27:09 -0400

    Enemy has to spot the player before moving.

--- Assets/Scripts/Characters/EnemyMovement.cs
+++ Assets/Scripts/Characters/EnemyMovement.cs
@@ -11,34 +11,53 @@ public class EnemyMovement : Character
 
 	bool isDead = false;
 
+	const float SPOT_DISTANCE = 5f;
 	const float HIT_DISTANCE = 0.5f;
+
+	const int DEATH_FORCE = 200;
 	
     // Update is called once per frame
     void FixedUpdate()
     {
-		if (isDead) {
-			// Don't Move
-		} else {
+		if (IsPlayerVisible() && !isDead) {
 			Chase();
 			CheckCollision();
-		}
-		
+		} 
     }
 
 	void Chase() {
 		rb.velocity = new Vector2(xMoveDirection, 0) * EnemySpeed;
 	}
 
+	// TODO: Refactor to use spherical collider
+	bool IsPlayerVisible() {
+		RaycastHit2D leftHit = Physics2D.Raycast(transform.position, Vector2.left, SPOT_DISTANCE);
+		RaycastHit2D rightHit = Physics2D.Raycast(transform.position, Vector2.right, SPOT_DISTANCE);
+		
+		return IsPlayer(leftHit) || IsPlayer(rightHit);
+	}
+
+	// Helper method to check to see if a hit is valid
+	bool IsValid(RaycastHit2D hit) {
+		return hit != null && hit.collider != null;
+	}
+
+	// Helper method to see if the player was hit
+	bool IsPlayer(RaycastHit2D hit) {
+		return IsValid(hit) && hit.collider.tag == "Player";
+	}
+
 	void CheckCollision() {
 
-		RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(xMoveDirection, 0));
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(xMoveDirection, 0), HIT_DISTANCE);
 		
-		if (hit != null && hit.collider != null && hit.distance < HIT_DISTANCE) {
+		if (IsValid(hit)) {
 			Flip();
-			if (hit.collider.tag == "Player") {
-				// Kill Player
-				hit.collider.GetComponent<PlayerHealth>().Die();
-			}
+		}
+
+		if (IsPlayer(hit)) {
+			// Kill Player
+			hit.collider.GetComponent<PlayerHealth>().Die();
 		}									 
 	}
 
@@ -53,7 +72,7 @@ public class EnemyMovement : Character
 	}
 
 	public void Die() {
-		rb.AddForce(Vector2.right * 200);
+		rb.AddForce(Vector2.right * DEATH_FORCE);
 		rb.freezeRotation = false;
 		rb.gravityScale = 8f;
 		GetComponent<BoxCollider2D>().enabled = false;
