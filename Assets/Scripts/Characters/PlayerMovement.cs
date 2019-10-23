3c8947e56651d5401831026e11b5b82fd4c11adc
Cidney Hamilton <cidney@cidneyhamilton.com>
2019-10-23 15:08:44 -0400

    Tweak player movement script.

--- Assets/Scripts/Characters/PlayerMovement.cs
+++ Assets/Scripts/Characters/PlayerMovement.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 using Cyborg.Platformer;
 
+
+// Handles movement and jumping for a character
 public class PlayerMovement : Character
 {
+	// The speed of the player's movement
 	public int Speed = 6;
+
+	// The amount of force in a jump
 	public int JumpForce = 1250;
 
-	private GroundChecker GroundChecker;
+	// Collider to check if the player is grounded
+	GroundChecker GroundChecker;
 	
 	protected override void Start() {
 		base.Start();
@@ -21,26 +27,13 @@ public class PlayerMovement : Character
 		Jump();
 	}
 
+	// Handles movement
 	void Move() {
-		// controls
 		float deltaX = Input.GetAxisRaw("Horizontal") * Speed;
-		
-		// animations
-		if (deltaX != 0 ) {
-			animator.SetBool("isWalking", GroundChecker.IsGrounded);
-		} else {
-			animator.SetBool("isWalking", false);
-		}
 
-		animator.SetBool("isJumping", !GroundChecker.IsGrounded);
+		UpdateAnimator(deltaX != 0, GroundChecker.IsGrounded);
 		
-		// player direction
-		if (deltaX < 0.0f && !sr.flipX) {
-			// Flip to the left
-			Flip();
-		} else if (deltaX > 0.0f && sr.flipX) {
-			Flip();
-		}
+		UpdateFlip(deltaX);
 		
 		// Update the velocity
 		rb.velocity = new Vector2(deltaX, rb.velocity.y);
@@ -56,11 +49,31 @@ public class PlayerMovement : Character
 		}
 	}
 
-	void Flip() {
-		sr.flipX = !sr.flipX;		
+	// Update the Animator to reflect the player's current state
+	void UpdateAnimator(bool isWalking, bool isGrounded) {
+		if (isWalking) {
+			animator.SetBool("isWalking", isGrounded);
+		} else {
+			animator.SetBool("isWalking", false);
+		}
+		
+		animator.SetBool("isJumping", !isGrounded);
+	}
+	
+	// Update the flip direction of the sprite
+	void UpdateFlip(float deltaX) {
+
+		if (deltaX < 0.0f && !sr.flipX ) {
+			// If moving left and not flipped
+			FlipSprite();
+		} else if (deltaX > 0.0f && sr.flipX) {
+			// If moving right and flipped
+			FlipSprite();	
+		}		
 	}
 
-
-
+	void FlipSprite() {
+		sr.flipX = !sr.flipX;					
+	}
 	
 }
